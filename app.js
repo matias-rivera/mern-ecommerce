02051785f65bb7512d3f5472061034ad110e01b3
Matías Rivera <juanmatiasrivera@outlook.com>
2020-10-10 19:21:45 -0300

    added braintree(payment method) routes and controller

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const authRoutes = require('./routes/auth')
 const userRoutes = require('./routes/user')
 const categoryRoutes = require('./routes/category')
 const productRoutes = require('./routes/product')
-
+const braintreeRoutes = require('./routes/braintree');
 
 //app
 const app = express();
@@ -38,6 +38,7 @@ app.use('/api',authRoutes);
 app.use('/api',userRoutes);
 app.use('/api',categoryRoutes);
 app.use('/api',productRoutes);
+app.use('/api',braintreeRoutes);
 
 //port
 const port = process.env.PORT || 5050;
